cfb2979571ae3a4b44ccdf2768ba222edc9d7282
Артем Фомичев <foarvad@gmail.com>
2019-03-04 22:26:01 +0300

    Added links

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
     <header style={headerStyle}>
         <h1>TodoList</h1>
+        <Link style={linkStyle} to="/">Home</Link>{ ' | ' }
+        <Link style={linkStyle} to="/about">About</Link>
     </header>
   )
 }
@@ -12,4 +15,8 @@ const headerStyle = {
     padding: '10px',
     textAlign: 'center',
     background: '#333'
+}
+
+const linkStyle = {
+    color: '#fff'
 }
