ea86711ad1cb8b1672c88dba1e6f497b080fee43
Foarvad <foarvad@gmail.com>
2019-03-10 13:50:10 +0300

    Working Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App.jsx";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-//serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
